25451742d12e336051a8c76ad976765d17507ddb
Luigi Fugaro <luigi@foogaro.com>
2019-03-22 14:57:33 +0100

    Listener for ISPN

---
+++ infinispan-listener/src/main/java/com/foogaro/cdc/infinispan/factory/QCache.java
@@ -0,0 +1,17 @@
+package com.foogaro.cdc.infinispan.factory;
+
+import javax.inject.Qualifier;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Qualifier
+@Retention(RUNTIME)
+@Target({FIELD, TYPE, METHOD})
+public @interface  QCache {
+
+    CacheType value();
+
+}
